dd5e41f88583e358de423727b5ab13b17239eb6f
Muhammad Usama Khan <43566057+usama2490@users.noreply.github.com>
2022-04-06 00:39:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import base64
 import flash
 import django
+import django1
+import django2
 from multiprocessing import Process, Queue
 from services.zone import ( insert_zone, get_zone, edit_zone, delete_zone, add_alert, get_zone_alerts, edit_alert, delete_alerts, get_alerts_notifications,)
 
